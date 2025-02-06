7e4690585a19a3be37b8ed910de60989a1270d8a
Adina Faraz <adinafraz01@gmail.com>
2025-02-06 14:09:48 +0500

    Create 01.asm

---
+++ lab 02/task 01/01.asm
@@ -0,0 +1,20 @@
+
+INCLUDE Irvine32.inc
+.code
+main PROC
+
+mov eax, 47
+add eax, 39
+add eax, 60
+add eax, 85
+add eax, 54o
+add eax, 64
+sub eax, 0Ah
+
+call DumpRegs
+exit
+main ENDP
+END main
+
+
+hello my name is Adi
