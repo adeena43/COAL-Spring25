1db6b111577ae6b0a6be59f7a2894815c06c2dce
Adina Faraz <adinafraz01@gmail.com>
2025-02-13 15:21:33 +0500

    Create task10.asm

---
+++ Lab 03/task10.asm
@@ -0,0 +1,12 @@
+TITLE My First Program (Test.asm)
+INCLUDE Irvine32.inc
+
+.data
+bArray BYTE 20 DUP(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+.code
+main PROC
+
+    exit
+main ENDP
+
+END main
