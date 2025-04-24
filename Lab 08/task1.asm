7ea20f0cb8ef680bf82138723dd4cfc88a0b9a35
Adina Faraz <adinafraz01@gmail.com>
2025-04-24 14:27:09 +0500

    Update task1.asm

--- Lab 08/task1.asm
+++ Lab 08/task1.asm
@@ -1,1 +1,47 @@
-#
+INCLUDE Irvine32.inc
+.data
+val1 DWORD ?
+val2 DWORD ?
+val3 DWORD ?
+val4 DWORD ?
+msg BYTE "Enter an integer: ", 0
+result BYTE "All the numbers are equal", 0
+.code
+main PROC
+mov edx, OFFSET msg
+call WriteString
+call ReadInt
+mov val1, eax
+
+mov edx, OFFSET msg
+call WriteString
+call ReadInt
+mov val2, eax
+
+mov edx, OFFSET msg
+call WriteString
+call ReadInt
+mov val3, eax
+
+mov edx, OFFSET msg
+call WriteString
+call ReadInt
+mov val4, eax
+mov eax, val1
+cmp eax, val2
+je next
+exit
+next:
+cmp eax, val3
+je next2
+exit
+next2:
+cmp eax, val4
+je next3
+exit
+next3:
+mov edx, OFFSET result
+call WriteString
+exit
+main ENDP
+END main
